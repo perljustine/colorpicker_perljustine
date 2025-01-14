c00a0490e7ebfa4f39621e288dcf00ac85adfc58
perljustine <justine.perl@hotmail.com>
2025-01-14 13:24:17 +0100

    ajout fonction

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -6,18 +6,46 @@ bar.classList.add('bar');
 body.appendChild(grid);
 body.appendChild(bar);
 
+let selectedColor = 0;
+const colors = [
+    "AliceBlue",
+    "AntiqueWhite",
+    "CornflowerBlue", 
+    "DarkSeaGreen", 
+    "SkyBlue", 
+    "MediumTurquoise", 
+    "PaleTurquoise", 
+    "lavender", 
+    "PowderBlue",
+];
+
 for (let i = 0; i < 5; i++) {
     for (let j = 0; j < 5; j++) {
-      const cell = document.createElement('div');
-      cell.classList.add('cell');
-      grid.appendChild(cell);
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.addEventListener('click', () => {
+            cell.style.backgroundColor = selectedColor;
+        });
+
+        cell.addEventListener('dblclick', () => {
+            cell.style.backgroundColor = '';
+        });
+        
+        grid.appendChild(cell);
     }
 }
 
+// Création de la barre de couleurs
 for (let i = 0; i < 9; i++) {
     const cell2 = document.createElement('div');
     cell2.classList.add('cell2');
+    cell2.style.backgroundColor = colors[i];
+    
+    cell2.addEventListener('click', () => {
+        selectedColor = colors[i];
+        
+    });
+    
     bar.appendChild(cell2);
 }
-
 
