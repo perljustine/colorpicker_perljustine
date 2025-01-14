039ecc7460f30007b4809b56674a1e4336491fc0
perljustine <justine.perl@hotmail.com>
2025-01-14 14:16:42 +0100

    modifs

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,10 +1,19 @@
 let body = document.querySelector('body');
 let grid = document.createElement('div');
 let bar = document.createElement('div');
+
+let Titre = document.createElement('h1');
+let texteTitre = document.createTextNode('Colorpicker du Swag');
+Titre.appendChild(texteTitre);
+body.appendChild(Titre);
+
 grid.classList.add('grid');
 bar.classList.add('bar');
 body.appendChild(grid);
 body.appendChild(bar);
+
+
+
 
 let selectedColor = 0;
 const colors = [
@@ -26,11 +35,9 @@ for (let i = 0; i < 5; i++) {
         cell.addEventListener('click', () => {
             cell.style.backgroundColor = selectedColor;
         });
-
         cell.addEventListener('dblclick', () => {
             cell.style.backgroundColor = '';
         });
-        
         grid.appendChild(cell);
     }
 }
@@ -40,12 +47,19 @@ for (let i = 0; i < 9; i++) {
     const cell2 = document.createElement('div');
     cell2.classList.add('cell2');
     cell2.style.backgroundColor = colors[i];
-    
     cell2.addEventListener('click', () => {
         selectedColor = colors[i];
-        
     });
-    
     bar.appendChild(cell2);
 }
 
+let clearbttn = document.createElement('button')
+clearbttn.textContent = "Clear"
+body.appendChild(clearbttn);
+
+
+clearbttn.addEventListener('click', () => {
+    document.querySelectorAll('.cell').forEach(cell => {
+        cell.style.backgroundColor ='';})
+
+})
